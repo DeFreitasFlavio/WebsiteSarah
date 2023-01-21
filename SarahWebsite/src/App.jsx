c08e1b81badd9609bcab7faaf3a0c6ce55c55592
Jean Gravian <defreitas.flavio7@gmail.com>
2023-01-21 19:54:20 +0100

    80%

--- SarahWebsite/src/App.jsx
+++ SarahWebsite/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/header";
-import Carousel from "./components/Carousel/Carousel";
+import Carousel from "./components/Carousel/carousel";
 import Details from "./components/Details/details";
 import Photo from "./components/photo/photo";
 import Footer from "./components/Footer/footer";
