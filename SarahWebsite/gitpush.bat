a1441a9e1d58421a6965d566393b2616d838a210
Jean Gravian <defreitas.flavio7@gmail.com>
2023-01-25 15:59:03 +0100

    Res

--- SarahWebsite/gitpush.bat
+++ SarahWebsite/gitpush.bat
@@ -1,8 +1,14 @@
 git add .
 
-git commit -m %1
+if "%1" == "" or "%1" < 4 (
+    echo "Please enter a commit message"
+) else (
+    git commit -m %1
 
-git push
+    git push
 
-color 06
+
+color 0d
 @echo "Done"
+)
+
